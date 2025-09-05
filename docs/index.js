03ddc07b8bd01af4cb8595fe129be5a9dd496403
jessefriend <35971997+jessefriend@users.noreply.github.com>
2025-09-05 15:38:24 +0200

    Document particle trail range

--- docs/index.js
+++ docs/index.js
@@ -7091,7 +7091,8 @@
               60,  "#f46d43",
               100, "#d53e4f"
             ],
-            "particle-trail": 1.0,
+            // Values above 1 leave longer, slower-fading trails (max 2)
+            "particle-trail": 1.5,
             "trail-substeps": 8
           }
         }
